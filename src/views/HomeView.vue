e91d68333759f6e80a71c5dddc1d22d00c6cdc9a
Mustafa Demir <mustafademir_md@hotmail.com>
2022-03-31 00:03:16 +0300

    Empty vue project with TailwindCSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Check Tailwind</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+<style scoped>
+ h1 {
+   @apply text-3xl text-red-500;
+ }
+</style>
